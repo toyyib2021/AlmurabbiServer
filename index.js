64cd81ec44fffd79bf91181154ec619a507b9a91
unknown <pureknowledge41@gmeil.com>
2023-07-01 04:46:18 -0700

    Almurabbi 11

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-const config = {
-  ktor: {
-    deployment: {
-      port: 8080,
-      port: process.env.PORT
-    },
-    application: {
-      modules: ['com.schooladmin.ApplicationKt.module']
-    }
-  },
-  jwt: {
-    domain: 'https://jwt-provider-domain/',
-    audience: 'jwt-audience',
-    realm: 'ktor sample app'
-  }
-};
+
+
+// Import required modules or define any necessary variables/constants
+
+// Define your application logic or server setup
+
+// Example: Create a simple HTTP server
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!');
+});
+
+const port = 8080; // Specify the port number
+
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
